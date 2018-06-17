bb403fa4f780a52f2b94db683c57ad8cf683a0ea
inatonix <imitation0813@gmail.com>
2018-06-18 04:58:37 +0900

    add lexer test

---
+++ lexer/lexer_test.go
@@ -0,0 +1,40 @@
+package lexer_test
+
+import (
+	"testing"
+
+	"github.com/monkey/token"
+)
+
+func TestNextToken(t *testing.T) {
+	input := `=+(){},;`
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.ASSIGN, "="},
+		{token.PLUS, "+"},
+		{token.LPAREN, "("},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RBRACE, "}"},
+		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("token type is not expected one")
+		}
+
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("token literal is not expected one")
+		}
+	}
+
+}
