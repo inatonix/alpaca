1d5cd42d4c2939ed5d4fcf5493e70e639dcfa8a0
inatonix <imitation0813@gmail.com>
2018-06-22 12:36:07 +0900

    add return

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -72,6 +72,34 @@ func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
 	return true
 }
 
+func TestReturnStatements(t *testing.T) {
+	input := `
+		return 5;
+		return 10;
+		return 993322;
+	`
+
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 3 {
+		t.Fatalf("program does not haev 3 statements")
+	}
+
+	for _, stmt := range program.Statements {
+		returnStmt, ok := stmt.(*ast.ReturnStatement)
+		if !ok {
+			t.Errorf("stmt not returnStatement")
+		}
+		if returnStmt.TokenLiteral() != "return" {
+			t.Errorf("returnStmt.Tokenliteral not 'return'")
+		}
+	}
+}
+
 func checkParserErrors(t *testing.T, p *Parser) {
 	errors := p.Errors()
 	if len(errors) == 0 {
